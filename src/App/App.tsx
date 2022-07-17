24511b3c1e47e0ccaf1d1966a7ebb8b048388234
toffee <antoinette.zu@icloud.com>
2022-07-17 23:24:43 +0300

    model works

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Logo } from 'assets';
+import React, { Suspense } from 'react';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import cnBind, { Argument } from 'classnames/bind';
-
-import { Counter } from 'components/Counter';
+import Heart from 'models/Heart';
 
 import styles from './App.module.scss';
 
@@ -11,10 +11,22 @@ const cx = cnBind.bind(styles) as (...args: Argument[]) => string;
 export const App: React.FC = () => {
     return (
         <div className={cx('App')}>
-            <header className={cx('App-header')}>
-                <Logo className={cx('App-logo')} />
-                <Counter title="THMOON" />
-            </header>
+            <Canvas
+                camera={{ position: [2, 0, 12.25], fov: 15 }}
+                style={{
+                    backgroundColor: '#111a21',
+                    width: '100vw',
+                    height: '100vh',
+                }}
+            >
+                <ambientLight intensity={1.25} />
+                <ambientLight intensity={0.1} />
+                <directionalLight intensity={0.4} />
+                <Suspense fallback={null}>
+                    <Heart />
+                </Suspense>
+                <OrbitControls />
+            </Canvas>
         </div>
     );
 };
